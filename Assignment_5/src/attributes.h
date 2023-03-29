e9ddfb70800875fcb09db0e830ecc9f957b2eece
Wentao Wang <wentao.wang@nyu.edu>
2023-03-29 17:09:07 -0400

    Done Ex.1: render a mesh to get a silhouette

--- Assignment_5/src/attributes.h
+++ Assignment_5/src/attributes.h
@@ -2,11 +2,9 @@
 
 #include <Eigen/Core>
 
-class VertexAttributes
-{
+class VertexAttributes {
 	public:
-	VertexAttributes(double x = 0, double y = 0, double z = 0, double w = 1)
-	{
+	VertexAttributes(double x = 0, double y = 0, double z = 0, double w = 1) {
 		position << x,y,z,w;
 	}
 
@@ -18,8 +16,7 @@ class VertexAttributes
         const double alpha, 
         const double beta, 
         const double gamma
-    ) 
-    {
+    ) {
         VertexAttributes r;
         r.position = alpha*a.position + beta*b.position + gamma*c.position;
         return r;
@@ -28,29 +25,24 @@ class VertexAttributes
 	Eigen::Vector4d position;
 };
 
-class FragmentAttributes
-{
+class FragmentAttributes {
 	public:
-	FragmentAttributes(double r = 0, double g = 0, double b = 0, double a = 1)
-	{
+	FragmentAttributes(double r = 0, double g = 0, double b = 0, double a = 1) {
 		color << r,g,b,a;
 	}
 
 	Eigen::Vector4d color;
 };
 
-class FrameBufferAttributes
-{
+class FrameBufferAttributes {
 	public:
-	FrameBufferAttributes(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 255)
-	{
+	FrameBufferAttributes(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 0) {
 		color << r,g,b,a;
 	}
 
 	Eigen::Matrix<uint8_t,4,1> color;
 };
 
-class UniformAttributes
-{
+class UniformAttributes {
 	public:
 };
--- Assignment_5/src/main.cpp
+++ Assignment_5/src/main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <getopt.h>
 
 // Utilities for the Assignment
 #include "raster.h"
@@ -10,13 +16,123 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION // Do not include this line twice in your project!
 #include "stb_image_write.h"
 
-using namespace std;
+// Shortcut to avoid Eigen:: everywhere, DO NOT USE IN .h
+using namespace Eigen;
 
-int main() 
-{
+////////////////////////////////////////////////////////////////////////////////
+// Classes for the scene
+////////////////////////////////////////////////////////////////////////////////
+
+typedef Vector4d Color; // RBGA Color (in [0, 1])
+
+const double eps = 1e-7, inf = 1./0., pi = acos(-1);
+
+template<class T>
+T sqr(const T x) {
+	return x * x;
+}
+
+
+enum ProjectionType {orthographic, perspective};
+const char *ProjectionTypeString[] = {"orthographic", "perspective"};
+
+
+class Light {
+public:
+	Vector3d position;
+	Color color;
+	Light(const Vector3d &position, const Color &color): position(position), color(color) {
+	}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Scene setup, global variables
+////////////////////////////////////////////////////////////////////////////////
+const std::string data_dir = DATA_DIR;
+std::string mesh_filename(data_dir + "bunny.off");
+bool refit_mesh = true;
+
+// Triangle Mesh
+MatrixX3d vertices; // n x 3 matrix (n points)
+MatrixX3i facets;   // m x 3 matrix (m triangles)
+
+//Material for the object, same material for all objects
+const Color obj_ambient_color(0.0, 0.5, 0.0, 0);
+const Color obj_diffuse_color(0.5, 0.5, 0.5, 0);
+const Color obj_specular_color(0.2, 0.2, 0.2, 0);
+double obj_specular_exponent = 256.0;
+const Color obj_reflection_color(0.7, 0.7, 0.7, 0);
+
+//Lights
+std::vector<Light> lights;
+//Ambient light
+const Color ambient_light(0.2, 0.2, 0.2, 0);
+
+//Fills the different arrays
+void setup_scene() {
+    //Loads file
+    std::ifstream in(mesh_filename);
+    std::string token;
+    in >> token;
+    int nv, nf, ne;
+    in >> nv >> nf >> ne;
+    vertices.resize(nv, 3);
+    facets.resize(nf, 3);
+    for (int i = 0; i < nv; ++i) {
+        in >> vertices(i, 0) >> vertices(i, 1) >> vertices(i, 2);
+    }
+    for (int i = 0; i < nf; ++i) {
+        int s;
+        in >> s >> facets(i, 0) >> facets(i, 1) >> facets(i, 2);
+        assert(s == 3);
+    }
+
+	//Refit the mesh into [-1, +1]^3
+	if (refit_mesh) {
+		Matrix<double, 1, 3>
+			min_coord = vertices.colwise().minCoeff(),
+			max_coord = vertices.colwise().maxCoeff(),
+			mid_coord = (min_coord + max_coord) / 2;
+		double resize_length = (max_coord - min_coord).maxCoeff();
+		vertices.rowwise() -= mid_coord;  // centering
+		vertices /= resize_length / 2;  // rescaling
+	}
+
+    //Lights
+	lights.emplace_back(Vector3d( 8,  8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d( 6, -8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d( 4,  8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d( 2, -8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d( 0,  8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d(-2, -8, 0), Color(16, 16, 16, 0));
+	lights.emplace_back(Vector3d(-4,  8, 0), Color(16, 16, 16, 0));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void render_scene(
+	// Camera settings
+	Vector3d camera_position,
+	ProjectionType projection_type,
+	double focal_length,
+	double field_of_view,
+	// Image size (h, w)
+	const size_t w,
+	const size_t h,
+	const std::string filename
+) {
+    std::cout << "Simple rendering system for triangulated 3D models based on rasterization." << std::endl;
+
+	// TODO: will only be used in perspective camera
+    // The camera always points in the direction -z
+    // The sensor grid is at a distance 'focal_length' from the camera center,
+    // and covers an viewing angle given by 'field_of_view'.
+    double aspect_ratio = double(w) / double(h);
+    double image_y = tan(field_of_view / 2) * focal_length;
+    double image_x = aspect_ratio * image_y;
 
 	// The Framebuffer storing the image rendered by the rasterizer
-	Eigen::Matrix<FrameBufferAttributes,Eigen::Dynamic,Eigen::Dynamic> frameBuffer(500,500);
+	Eigen::Matrix<FrameBufferAttributes,Eigen::Dynamic,Eigen::Dynamic> frameBuffer(w, h);
 
 	// Global Constants (empty in this example)
 	UniformAttributes uniform;
@@ -25,34 +141,116 @@ int main()
 	Program program;
 
 	// The vertex shader is the identity
-	program.VertexShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
+	program.VertexShader = [](const VertexAttributes& va, const UniformAttributes& uniform) {
 		return va;
 	};
 
 	// The fragment shader uses a fixed color
-	program.FragmentShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
-		return FragmentAttributes(1,0,0);
+	program.FragmentShader = [](const VertexAttributes& va, const UniformAttributes& uniform) {
+		return FragmentAttributes(1, 1, 1);
 	};
 
 	// The blending shader converts colors between 0 and 1 to uint8
-	program.BlendingShader = [](const FragmentAttributes& fa, const FrameBufferAttributes& previous)
-	{
+	program.BlendingShader = [](const FragmentAttributes& fa, const FrameBufferAttributes& previous) {
 		return FrameBufferAttributes(fa.color[0]*255,fa.color[1]*255,fa.color[2]*255,fa.color[3]*255);
 	};
 
 	// One triangle in the center of the screen
-	vector<VertexAttributes> vertices;
-	vertices.push_back(VertexAttributes(-1,-1,0));
-	vertices.push_back(VertexAttributes(1,-1,0));
-	vertices.push_back(VertexAttributes(0,1,0));
+	std::vector<VertexAttributes> triangle_vertices;
+	const size_t nf = facets.rows();
+	for (size_t i = 0; i < nf; ++i) {
+		for (size_t k = 0; k < 3; ++k) {
+			const Vector3d position = vertices.row(facets(i, k));
+			triangle_vertices.push_back(VertexAttributes(position(0), position(1), position(2)));
+		}
+	}
 
-	rasterize_triangles(program,uniform,vertices,frameBuffer);
+	rasterize_triangles(program, uniform, triangle_vertices, frameBuffer);
 
-	vector<uint8_t> image;
+	std::vector<uint8_t> image;
 	framebuffer_to_uint8(frameBuffer,image);
-	stbi_write_png("triangle.png", frameBuffer.rows(), frameBuffer.cols(), 4, image.data(), frameBuffer.rows()*4);
-	
-	return 0;
+	stbi_write_png(filename.c_str(), frameBuffer.rows(), frameBuffer.cols(), 4, image.data(), frameBuffer.rows()*4);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[]) {
+	std::ios::sync_with_stdio(false);
+
+	static struct option long_options[] = {
+		{"filename",              required_argument, 0, 0  },
+		{"mesh_filename",         required_argument, 0, 6  },
+		{"no_refit_mesh",         no_argument,       0, 7  },
+		{"focal_length",          required_argument, 0, 'f'},
+		{"field_of_view",         required_argument, 0, 1  },
+		{"projection_type",       required_argument, 0, 'p'},
+		{"obj_specular_exponent", required_argument, 0, 2  },
+		{"grid_size",             required_argument, 0, 'g'},
+		{0,                       0,                 0, 0  }
+	};
+
+	//Camera settings
+	const Vector3d camera_position(0, 0, 2);
+	ProjectionType projection_type = perspective;
+	double focal_length = 2;
+	double field_of_view = pi / 4; // 45 degrees
+	size_t w = 500, h = 500;
+	std::string filename("triangle.png");
+
+	int opt;
+	while ((opt = getopt_long(argc, argv, "w:h:f:p:", long_options, NULL)) != -1) {
+		switch (opt) {
+		case 0:
+			filename = optarg;
+			break;
+		case 6:
+			mesh_filename = optarg;
+			break;
+		case 7:
+			refit_mesh = false;
+			break;
+		case 1:
+			field_of_view = pi / 180 * atoi(optarg); // field of view in degree
+			break;
+		case 2:
+			obj_specular_exponent = atof(optarg);
+			break;
+		case 'w':
+			w = atoi(optarg);
+			break;
+		case 'h':
+			h = atoi(optarg);
+			break;
+		case 'f':
+			focal_length = atof(optarg);
+			break;
+		case 'p':
+			if (optarg[0] == 'p') {
+				projection_type = perspective;
+			}
+			else if (optarg[0] == 'o') {
+				projection_type = orthographic;
+			}
+			else {
+				std::cerr << "Unknown projection type: " << optarg << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			break;
+		default: // '?'
+			std::cerr << "Read the code for usage." << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+
+    setup_scene();
+
+    render_scene(
+		camera_position,
+		projection_type,
+		focal_length,
+		field_of_view,
+		w, h,
+		filename);
+
+    return 0;
 }
